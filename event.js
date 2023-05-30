f048116b4097ad38748f14444bc48d47c9b0e340
Sachini Anjana <97577090+NSachini@users.noreply.github.com>
2023-05-30 13:30:22 +0530

    Update
    Web Development - 1. Web Design for Beginners| Lesson 4: Fundamentals of JavaScript| 4.5 Handling Events in JavaScript

---
+++ event.js
@@ -0,0 +1,14 @@
+function mouseOver() { document.getElementById("demo").style.color = "red";
+}
+function mouseOut() {
+document.getElementById("demo").style.color = "black";
+}
+function bigImg(x) {
+  x.style.height = "140px";
+  x.style.width = "140px";
+}
+
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
